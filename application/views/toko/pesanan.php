824a033902c33eeda0af0d38c3c2f084d2b80961
RIDWANKING11 <muhammadridwanking@gmail.com>
2021-12-21 22:32:54 +0700

    memperbaiki daftar pesanan penjual

--- application/views/toko/pesanan.php
+++ application/views/toko/pesanan.php
@@ -3,8 +3,7 @@
         <nav id="sidebar" class="mt-5 rounded" style="position: fixed;">
             <div class="p-4">
                 <div class="d-flex">
-                    <img src="<?= base_url("assets/img/profile.jpg"); ?>" class="rounded-circle" alt="" width="80"
-                        height="80">
+                    <img src="<?= base_url("assets/img/profile.jpg"); ?>" class="rounded-circle" alt="" width="80" height="80">
                     <div class="ms-3 pt-2">
                         <h5 class="fw-bold">TOKOMIRAHSAE</h5>
                         <i class="far fa-star fa-lg"></i>
@@ -46,20 +45,17 @@
                     </li>
                     <li class="mb-2 dropdown-produk">
                         <i class="fas fa-box-open"></i>
-                        <a class="dropdown-toggle text-decoration-none fw-bold text-dark" data-bs-toggle="collapse"
-                            href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                        <a class="dropdown-toggle text-decoration-none fw-bold text-dark" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
                             Produk
                         </a>
                         <div class="collapse" id="collapseExample">
                             <div class="">
                                 <ul>
                                     <li>
-                                        <a href="<?= base_url("toko/tambah"); ?>"
-                                            class="text-decoration-none text-dark">Tambah Produk</a>
+                                        <a href="<?= base_url("toko/tambah"); ?>" class="text-decoration-none text-dark">Tambah Produk</a>
                                     </li>
                                     <li>
-                                        <a href="<?= base_url("toko/produk"); ?>"
-                                            class="text-decoration-none text-dark">Daftar Produk</a>
+                                        <a href="<?= base_url("toko/produk"); ?>" class="text-decoration-none text-dark">Daftar Produk</a>
                                     </li>
                                 </ul>
                             </div>
@@ -67,16 +63,14 @@
                     </li>
                     <li>
                         <i class="fas fa-box-open"></i>
-                        <a class="dropdown-toggle text-decoration-none fw-bold text-dark" data-bs-toggle="collapse"
-                            href="#pesanan" role="button" aria-expanded="false" aria-controls="collapseExample">
+                        <a class="dropdown-toggle text-decoration-none fw-bold text-dark" data-bs-toggle="collapse" href="#pesanan" role="button" aria-expanded="false" aria-controls="collapseExample">
                             Pesanan
                         </a>
                         <div class="collapse" id="pesanan">
                             <div class="">
                                 <ul>
                                     <li>
-                                        <a href="<?= base_url("toko/pesanan"); ?>"
-                                            class="text-decoration-none text-dark">Pesanan Saya</a>
+                                        <a href="<?= base_url("toko/pesanan"); ?>" class="text-decoration-none text-dark">Pesanan Saya</a>
                                     </li>
                                     <li>
                                 </ul>
@@ -105,16 +99,16 @@
             <tbody>
                 <?php $i = 1; ?>
                 <?php $n = 0; ?>
-                <?php forEach($pesanan as $p): ?>
-                <tr>
-                    <th scope="row"><?= $i++; ?></th>
-                    <td><?= $novel[$n++]["judul"]; ?></td>
-                    <td><?= $p["stok"]; ?></td>
-                    <td><?= $p["harga"]; ?></td>
-                    <td><?= $p["penerima"]; ?></td>
-                    <td>14 desember 2021</td>
-                    <td><?= $p["status"]; ?></td>
-                </tr>
+                <?php foreach ($pesanan as $p) : ?>
+                    <tr>
+                        <th scope="row"><?= $i++; ?></th>
+                        <td><?= $novel[$n++]["judul"]; ?></td>
+                        <td><?= $p["stok"]; ?></td>
+                        <td><?= $p["harga"]; ?></td>
+                        <td><?= $p["penerima"]; ?></td>
+                        <td>14 desember 2021</td>
+                        <td><?= $p["status"]; ?></td>
+                    </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
